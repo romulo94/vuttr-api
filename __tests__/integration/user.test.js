e9779b41c1c3ae8a6685c17daa1c8229da962f5b
Romulo Rocha <romulorocha063@gmail.com>
2019-10-05 18:30:07 -0300

    test to create user part 2

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,8 +1,14 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
-  it('it should be able to register', async () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
@@ -13,5 +19,27 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Rômulo Rocha',
+        email: 'romulorocha063@gmail.com',
+        password: '123',
+        password_hash: '123',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Rômulo Rocha',
+        email: 'romulorocha063@gmail.com',
+        password: '123',
+        password_hash: '123',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
