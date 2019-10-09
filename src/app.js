f71c8bc6e5d278d86528185bdd87f3c26957e583
Romulo Rocha <romulorocha063@gmail.com>
2019-10-09 00:19:30 -0300

    adding sentry

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import './bootstrap';
 
+import cors from 'cors';
 import Youch from 'youch';
 import express from 'express';
-import 'express-async-errors';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
+import 'express-async-errors';
 import routes from './routes';
 
 import './database';
@@ -12,17 +15,22 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
     this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(cors());
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
